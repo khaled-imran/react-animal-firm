8ebcd82be888401b61dc2e61acbc21163d6a439f
khaled <khaled@nascenia.com>
2023-12-02 18:20:14 +0600

    react first project

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [animals, setAnimals] = useState([]);
+
+  useEffect(() => { 
+    const lastQuery = localStorage.getItem('lastQuery');
+    search(lastQuery); 
+  }, []); // probely it wil call when it is monuted
+
+  const search = async (q) => {
+    const response = await fetch(
+      'http://localhost:8080?' + new URLSearchParams({ q })
+    );
+
+    const data = await response.json();
+    setAnimals(data);
+
+    localStorage.setItem('lastQuery', q);
+  };
+
+  return (
+    <main>
+      <input type='text'
+        placeholder='search'
+        onChange={(e) => search(e.target.value)}
+      />
+
+      <ul>
+        {animals.map((animal) => (
+          <Animal key={animal.id} {...animal} />
+        ))}
+      </ul>
+
+      {animals.length === 0 && 'No animals found' }
+    </main>
+  )
+}
+
+function Animal({ type, name, age }) {
+  return (
+    <li>
+      <strong>{type}</strong> {name} - {age}
+    </li>
+  );
+}
+
+export default App
